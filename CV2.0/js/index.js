b7e8f1d72ecd4903b91c6d5d11740f3059fa2b82
Cenyai <carlosyygg@gmail.com>
2023-05-05 17:03:03 +0200

    cambio en los botones

--- CV2.0/js/index.js
+++ CV2.0/js/index.js
@@ -45,9 +45,5 @@ window.onload = () => {
     }
 }
 
-window.onload = () => {
-    document.getElementById("Back").addEventListener("click", function () {
-        window.location.href = "https://cenyai.github.io/Unday/"; // Reemplaza "index.html" con la URL de tu página principal
-    });
-}
+
 
--- aficiones/js/index.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    document.getElementById("Back").addEventListener("click", function () {
-        window.location.href = "https://cenyai.github.io/Unday"; // Reemplaza "index.html" con la URL de tu página principal
-    });
-}
